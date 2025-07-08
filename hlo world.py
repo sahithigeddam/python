847d40f0e1f5d617c61e39a64bbec57cfeb95f8d
sahithigeddam <sahithigeddam1495@gmail.com>
2025-07-08 12:07:43 +0530

    Add files via upload

---
+++ hlo world.py
@@ -0,0 +1,73 @@
+#print('hlo world')
+
+#name='sahithi'
+#print('hlo'+'  '+name)
+
+#name = input('enter name:')
+#print("hlo"+ ' '+name)
+
+#a =10
+#b =5
+#print(a+b)
+
+a=int(input())
+b=int(input())
+print(a*b)
+
+#Print(a/b)
+
+#s=input()
+#n=int(input())
+#r = ""
+#for i in range(n):
+ #   n1=int(input())
+  #  r += s[n1]
+
+#s=input()
+#v="aeiouAEIOU"
+#for i in s:
+# if i in v:
+#        print(i,end="")
+
+'''string and integer combain'''
+# a=input("enter name:")
+# b=int(input("enter age:"))
+# print("hello", a , "you are", b ,"years old")
+
+"'string display'"
+#a =int(input("enter no"))
+# print(a)
+# print(str(a))
+#print(type(a))
+
+# a=input("enter your BRANCH:")
+# b=int(input("enter your ROLL  no:"))
+# c=int(input("enter your age:"))
+# d=input("enter your full name:")
+# print( d , c , a , b )
+
+'''print natural numbetrs'''
+# a = int(input())
+# sum = 0
+# for i in range(1,n+1):
+#        sum += i
+#        print(s)
+
+'''sum of even numbers up to numbers'''
+# t=int(input('enter a number:'))
+# total=0
+# for i in range(t+1):
+#     if i%2==0:
+#         total=total+i
+# print(total)
+
+'''get the first n characters from the given  string'''
+# word=input()
+# n = int(input())
+# print(word[:n])
+
+'''lucky number'''
+# a = int(input())
+# b = int(input())
+ 
+'''avg'''
